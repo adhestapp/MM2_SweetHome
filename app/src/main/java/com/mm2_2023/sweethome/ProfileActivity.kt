d3a78ea12b34932c0ade1cf240104f06d68eb899
adhestapp <19102106@ittelkom-pwt.ac.id>
2023-07-30 19:50:20 +0700

    UI

---
+++ app/src/main/java/com/mm2_2023/sweethome/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.mm2_2023.sweethome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
